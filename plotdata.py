fec3b7e38686a5e6df2630986a47c10ddd91c257
Shyam Srinivasan <shyamsweth@gmail.com>
2022-01-12 15:42:06 +0530

    scatter plot of temp/humidity/heat index

--- plotdata.py
+++ plotdata.py
@@ -4,8 +4,23 @@ import matplotlib.pyplot as plt
 def scatterplot(data, dtype, sep=False):
     """scatter plot of temperature/humidity/heat index"""
     if sep:
-        pass
+        if dtype == 'all':
+            fig, ax = plt.subplots(3, 1)
+            dtype = ['Temperature', 'Humidity', 'Heat Index']
+            for id, itype in enumerate(dtype):
+                ax[id].plot(data['Time'], data[itype], ls=None, lw=0.0, marker='.')
+                ax[id].set_ylabel(itype)
+            ax[2].set_xlabel('Date/Time')
+        else:   # len(dtype) = 1
+            fig, ax = plt.subplots(1, 1)
+            ax.plot(data['Time'], data[dtype], ls=None, lw=0.0, marker='.')
+            ax.set_ylabel(dtype)
+            ax.set_xlabel('Date/Time')
     else:
-        a = plt.plot(data['Time'], data[dtype], ls=None, lw=0.0, marker='.')
-        print('Complete')
+        fig, ax = plt.subplots(1, 1)
+        ax.plot(data['Time'], data[dtype], ls=None, lw=0.0, marker='.')
+        ax.set_xlabel('Date/Time')
+        ax.set_ylabel(dtype)
+    fig.show()
+    print('Complete')
     return
